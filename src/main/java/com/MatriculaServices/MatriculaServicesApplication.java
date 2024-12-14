31f4a30ad6d6e73978cd220eb685fa2cc7598643
CerocuelCode <CuelCipherX@outlook.com>
2024-12-14 15:39:29 -0500

    First Commit
    Creación del Proyecto

---
+++ src/main/java/com/MatriculaServices/MatriculaServicesApplication.java
@@ -0,0 +1,13 @@
+package com.MatriculaServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculaServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculaServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MatriculaServices/MatriculaServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MatriculaServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatriculaServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
